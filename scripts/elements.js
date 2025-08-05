616ffdd56fba12b1ce500f165f15dad7df2648bc
Ahmed Okasha <95878292+hmdOkasha@users.noreply.github.com>
2025-08-05 16:40:25 +0300

    branch testing
    branch

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,3 +1,5 @@
+// branches testing
+
 const darkModeBtn = document.querySelector('.DarkThemeToggle')
 const main = document.querySelector('.App');
 const taskList = document.querySelector('.TaskList__list');
